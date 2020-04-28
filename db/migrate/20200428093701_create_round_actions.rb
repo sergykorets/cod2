774a8489d7ce8ecc073cf4a7c62ebd0f92fc663e
Sergii Fedorchuk <ext-sergii.fedorchuck@foodie.fm>
2020-04-28 16:46:12 +0300

    fix indeces

--- db/migrate/20200428093701_create_round_actions.rb
+++ db/migrate/20200428093701_create_round_actions.rb
@@ -1,11 +1,11 @@
 class CreateRoundActions < ActiveRecord::Migration[5.1]
   def change
-    # create_table :round_actions do |t|
-    #   t.integer :user_id, index: true
-    #   t.integer :round_id, index: true
-    #   t.integer :round_action_type
-    #   t.integer :time
-    #   t.timestamps
-    # end
+    create_table :round_actions do |t|
+      t.integer :user_id, index: true
+      t.integer :round_id, index: true
+      t.integer :round_action_type
+      t.integer :time
+      t.timestamps
+    end
   end
 end
