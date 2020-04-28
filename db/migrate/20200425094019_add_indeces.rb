7342d28301c69705a0a322d7ee23c07efed16bc0
Sergii Fedorchuk <ext-sergii.fedorchuck@foodie.fm>
2020-04-28 14:07:53 +0300

    index fix

--- db/migrate/20200425094019_add_indeces.rb
+++ db/migrate/20200425094019_add_indeces.rb
@@ -1,6 +1,6 @@
 class AddIndeces < ActiveRecord::Migration[5.1]
   def change
-    add_index :actions, :damaged_user_idra
+    add_index :actions, :damaged_user_id
     add_index :actions, :damaging_user_id
     add_index :actions, :weapon_id
     add_index :actions, :round_id
